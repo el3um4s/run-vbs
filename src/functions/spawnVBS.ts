9348e6bbc601d15624f1487e07cfdb74aef62874
Samuele de Tomasi <el3um4s@gmail.com>
2022-07-13 17:20:54 +0200

    deleted unesed vars

--- src/functions/spawnVBS.ts
+++ src/functions/spawnVBS.ts
@@ -1,21 +1,4 @@
 import { spawn, SpawnOptionsWithoutStdio } from "node:child_process";
-import { Buffer } from "buffer";
-
-const isEmpty = (object: Record<string, unknown>): boolean =>
-  Object.keys(object).length === 0;
-
-const exitMessages: Record<number, string> = {
-  1: "Uncaught Fatal Exception",
-  3: "Internal JavaScript Parse Error",
-  4: "Internal JavaScript Evaluation Failure",
-  5: "Fatal Error",
-  6: "Non-function Internal Exception Handler",
-  7: "Internal Exception Handler Run-Time Failure",
-  9: "Invalid Argument",
-  10: "Internal JavaScript Run-Time Failure",
-  12: "Invalid Debug Argument",
-};
-
 interface Cmd {
   command: string;
   args: string[];
